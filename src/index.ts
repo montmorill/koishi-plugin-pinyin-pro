7ca366cc17842f225a0f400ca1c799388713d0ed
montmorill <montmorill@outlook.com>
2026-06-27 01:12:50 +0800

    bump version to 1.0.1 and expand PinyinProService interface with additional methods

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,22 @@ class PinyinProService extends Service {
 }
 
 export default Object.assign(PinyinProService.prototype, pinyinPro)
+
+interface PinyinProService {
+  getInitialAndFinal: typeof pinyinPro.getInitialAndFinal
+  getFinalParts: typeof pinyinPro.getFinalParts
+  getNumOfTone: typeof pinyinPro.getNumOfTone
+  pinyin: typeof pinyinPro.pinyin
+  customPinyin: typeof pinyinPro.customPinyin
+  clearCustomDict: typeof pinyinPro.clearCustomDict
+  addDict: typeof pinyinPro.addDict
+  removeDict: typeof pinyinPro.removeDict
+  match: typeof pinyinPro.match
+  html: typeof pinyinPro.html
+  polyphonic: typeof pinyinPro.polyphonic
+  convert: typeof pinyinPro.convert
+  segment: typeof pinyinPro.segment
+  OutputFormat: typeof pinyinPro.OutputFormat
+  addTraditionalDict: typeof pinyinPro.addTraditionalDict
+  getTraditionalDict: typeof pinyinPro.getTraditionalDict
+}
